6f69c7992fa0093365290286dddd3ae7de38ef91
Samet Sahin <a.sahin.ual@gmail.com>
2020-02-27 10:37:02 +0300

    Replaced images which had flaws

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
   static const dummy_image_url = 'https://about.canva.com/wp-content/uploads/sites/3/2016/08/logos-1.png';
   static const text_below_the_banner = 'Gül suyu; gül bitkisinin taç yapraklarının damıtılmasıyla elde edilen hidrosollerdir.\n\nGül suyu hamur işi ve lokum yapımında da kullanılır ve 1 litre gül suyu ortalama 6 bin gülden elde edilir.\n\nBuharalı tıp bilgini İbni Sina ve sonrasındaki hekimler ferahlatıcı ve dinlendirici özelliği nedeniyle ruhsal hastalıkların tedavisinde gül suyu kullanırdı.\n\nİçeriğinde geraniol, rodinol, eugenol, citronel ve feniletilalkol gibi ilaçlarda kullanılan bileşenler bulunan gül bitkisinden elde edilen gül suyu, günümüzde de bazı hastalıkların tedavisinde kullanılıyor. Boğaz ve bademcik iltihaplarını gidermede etkili olduğu bilinen gül suyu ayrıca antiseptikler arasında yer alıyor.';
-  static const footer_text = 'NEWROSE KOZMETİK SAN. VE TİC.LTD.ŞTİ.\n\nSULTANTEPE MAH. SELVİLİK CAD. NO:22/10ÜSKÜDAR - İSTANBUL\n\nTEL: 0 212 522 59 61\n\nE-mail: info@newrose.com.tr';
+  static const footer_text = 'NEWROSE KOZMETİK SAN. VE TİC.LTD.ŞTİ.\n\nSULTANTEPE MAH. SELVİLİK CAD. NO:22/10\nÜSKÜDAR - İSTANBUL\n\nTEL: 0 212 522 59 61\n\nE-mail: info@newrose.com.tr';
 }
